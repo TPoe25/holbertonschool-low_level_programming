0066837d2b4767b53bfd75ada2ab1a98fc0a8906
TPoe25 <t_poe25@yahoo.com>
2023-05-24 14:12:56 -0700

    4-puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main: writing c program that prints line using puts
+ * main- writing c program that prints line using puts
  *
- * return: always 0
- *
+ * return: always (0)
  */
 
 int main(void)
