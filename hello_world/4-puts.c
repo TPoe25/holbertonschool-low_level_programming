39686eb9b2efe0952caa1f450f43cced2f5a0c8c
TPoe25 <t_poe25@yahoo.com>
2023-05-24 13:33:43 -0700

    4-puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+/**
+ * main: writing c program that prints line using puts
+ *
+ * return: always 0
+ *
+ */
 
 int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle\n";
+	char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(Mystr);
 
