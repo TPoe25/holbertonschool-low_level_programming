1073b91f059c52b35376d55876b6a8f8b3f524f7
TPoe25 <t_poe25@yahoo.com>
2023-05-25 13:41:15 -0700

    2 print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints letters
+ * Return: Always (0)
+ * description: prints the alphabet in lowercase
+ */
+
+int main (void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+
+		alphabet++;
+	}
+
+	putchar('\n');
+	return (0);
+}
