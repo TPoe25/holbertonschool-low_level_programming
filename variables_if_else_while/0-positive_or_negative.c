206b1d35f7c9d122178a04d10fb430d83efe6d43
TPoe25 <t_poe25@yahoo.com>
2023-05-25 12:21:12 -0700

    0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * Main - returning random numbers
  * Return: Always (0)
- * Description: using an if statement to put out random numbers
+ * description: using an if statement to put out random numbers
  **/
 
 int main(void)
@@ -16,14 +16,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	if (n > 0) { 
+	if (n > 0)
+	{
 		printf("%i is positive\n", n);
 	}
-	else if ((n = 0)) {
+	else if ((n = 0))
+	{
 		printf("%i is zero\n", n);
 	}
-	else if (n < 0) {
+	else if (n < 0)
+	{
 	       printf("%i is negative\n", n);
-	}	       
+	}
 	return (0);
 }
