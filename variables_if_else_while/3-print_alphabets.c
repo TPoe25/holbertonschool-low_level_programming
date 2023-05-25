4874bb6a811606f5a0f1b0412a1682544d609d75
TPoe25 <t_poe25@yahoo.com>
2023-05-25 14:40:35 -0700

    3-print alphabet

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - prints letters
+ * Return: Always (0)
+ * description: prints upper and lowercase letters
+ **/
+
+int main(void)
+{
+	char x;
+
+	for ( x = 'a'; x<= 'z'; x++)
+		putchar(x);
+	for ( x = 'A'; x <= 'Z'; x++)
+		putchar(x);
+	
+	putchar('\n');
+
+return (0);
+}
