ab09c8ed348b4a79ea62d740dabb11a47fabc21d
TPoe25 <t_poe25@yahoo.com>
2023-05-26 11:59:57 -0700

    4alphabt

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,17 +9,15 @@
 int main(void)
 {
 	char alphabet = 'a';
-	
-	while (alphabet <= 'z')
-	
-	if ((alphabet!='e')&&(alphabet!='q'))
-	{
-		
-		putchar(alphabet);
 
-		alphabet++;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if ((alphabet != 'e') && (alphabet != 'q'))
+
+			putchar(alphabet);
 
 	}
+	
 	putchar('\n');
 
 	return (0);
