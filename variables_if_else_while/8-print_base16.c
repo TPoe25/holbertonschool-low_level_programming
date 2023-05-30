a029a875ce9858b3af0c7d90e2a4a63db5e6b06f
TPoe25 <t_poe25@yahoo.com>
2023-05-30 08:08:31 -0700

    8-printbase16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - base 16
+ * Return: always (0)
+ * description: using %x to convert base 16 to hexidecimal
+ **/
+
+int main(void)
+{
+	int num;
+	
+	for (num = 1; num < 17; num++)
+	
+	{	
+	
+	scanf(int num);
+	
+	putchar("%x");
+
+	putchar(int num);
+	
+	}
+
+	putchar('\n');
+
+	return (0);
+}
