889e4eac43f47220e8e5a8b8d2cc96d6318c0fd1
TPoe25 <t_poe25@yahoo.com>
2023-05-30 12:48:49 -0700

    1-alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main -
+ * description:
+ * Return: Always (0)
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
