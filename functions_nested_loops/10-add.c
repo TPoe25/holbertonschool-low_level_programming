7b1d960de5963927f60a16966d9c92750d7c05a5
TPoe25 <t_poe25@yahoo.com>
2023-05-30 17:32:04 -0700

    10-add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds the intergers
+ * @a: first
+ * @b: second
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
