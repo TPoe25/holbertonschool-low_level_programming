c8e3e9f91af1e1a584cb2960007f6b5ae5228706
TPoe25 <t_poe25@yahoo.com>
2023-06-07 15:11:00 -0700

    1-strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,20 +9,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int x = 0;
-	int j = 0;
+	int x;
+	int j;
 
+	x = 0;
 	while (dest[x] != '\0')
 	{
 		x++;
 	}
-	while (src[j] != '\0')
-	{
-		dest[x] = src[j];
-		x++;
-		j++;
-	}
-	if (src[n] > '\0')
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
 		dest[x] = src[j];
 		x++;
