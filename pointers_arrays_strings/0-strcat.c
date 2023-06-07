df1cae787825e852db04ac57cd3da8678a08211b
TPoe25 <t_poe25@yahoo.com>
2023-06-07 13:29:42 -0700

    0-strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat -concatenates two strings
+ * @dest: string
+ * @src: string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int x=0;
+	int j=0;
+
+	while(dest[x] != '\0')
+	{
+		++dest;
+		x++;
+	}
+	while(src[j] != '\0')
+	{
+		dest=src;
+		dest++;
+		src++;
+	}
+	return (dest);
+}
