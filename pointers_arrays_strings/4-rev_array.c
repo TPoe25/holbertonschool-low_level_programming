e91122d6aa1a71a37065c58abd2046ff9111b4a8
TPoe25 <t_poe25@yahoo.com>
2023-06-09 14:21:20 -0700

    4-rev

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,7 @@
 
 /**
  * reverse_array - reverses content of an array of ints
- * @*a: int pointer
+ * @a: int pointer
  * @n: interger
  * Return: 0
  */
@@ -14,9 +14,9 @@ void reverse_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n--; i++)
-		{
-			temp = a[i];
-			a[i] = a[n];
-			a[n] = temp;
-		}
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+	}
 }
