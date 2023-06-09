ca7b9d4039b0661e9b0b6385889894366a734555
TPoe25 <t_poe25@yahoo.com>
2023-06-09 14:16:35 -0700

    4 rev

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * reverse_array - reverses content of an array of ints
@@ -9,10 +10,13 @@
 
 void reverse_array(int *a, int n)
 {
-	while (a[n] != '\0' && a[n] > 0)
-	{
-		a++;
-		a--;
-	}
-	a--;
+	int temp;
+	int i;
+
+	for (i = 0; i < n--; i++)
+		{
+			temp = a[i];
+			a[i] = a[n];
+			a[n] = temp;
+		}
 }
