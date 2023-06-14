8bb993eb6b2b817703f3ae73797ee8299d9c53a1
TPoe25 <t_poe25@yahoo.com>
2023-06-14 13:51:02 -0700

    3factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: input
+ * Return: 0
+ */
+
+int factorial(int n)
+{
+	if (n <= 1)
+	{
+		return (1);
+	}
+	return n * factorial(n - 1);
+}
