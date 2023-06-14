8544caa123061edf263f1701b014f2ff07761f10
TPoe25 <t_poe25@yahoo.com>
2023-06-14 14:33:21 -0700

    fact

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -8,9 +8,17 @@
 
 int factorial(int n)
 {
+	int fact;
+
 	if (n <= 1)
 	{
 		return (1);
 	}
-	return n * factorial(n - 1);
+	else
+	{
+		fact = n * factorial(n - 1);
+	}
+	return fact;
+	if (n < 0)
+	return (-1);
 }
