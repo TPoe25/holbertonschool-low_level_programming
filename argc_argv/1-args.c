373860e4b97b37de9477be525da4866a7f9fd482
TPoe25 <t_poe25@yahoo.com>
2023-06-16 12:44:13 -0700

    push

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+	int i;
+	printf("%d\n", argc);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+	}
+	exit(EXIT_SUCCESS);
+}
