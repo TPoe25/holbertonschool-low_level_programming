5facc04655844b605387c43cdce67cc3808001d3
TPoe25 <t_poe25@yahoo.com>
2023-06-26 11:52:08 -0700

    0-create

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,11 +12,11 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int length;
 
-	s = malloc(size * sizeof(char));
-	if (s == NULL)
+	if (size == 0)
 	{
-		return (0);
+		return (NULL);
 	}
+	s = malloc(size * sizeof(char));
 	length = 0;
 	while (length < size)
 	{
