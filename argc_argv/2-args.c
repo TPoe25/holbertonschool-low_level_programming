a64a7cc613e7afa4b742927dd38d7312f90f4993
TPoe25 <t_poe25@yahoo.com>
2023-06-27 16:26:07 -0700

    2args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
