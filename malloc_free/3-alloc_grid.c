1af77432af8f1237e72d35de0cbe9df99252d737
TPoe25 <t_poe25@yahoo.com>
2023-06-28 13:36:57 -0700

    4-free

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -10,30 +10,30 @@
 
 int **alloc_grid(int width, int height)
 {
-        int **array, i, x;
-        int length = width * height;
+	int **array, i, x;
+	int length = width * height;
 
-        if (length <= 0)
+	if (length <= 0)
+	return (NULL);
+
+	array = (int **)malloc(sizeof(int *) * height);
+
+	if (array == NULL)
         return (NULL);
 
-        array = (int **)malloc(sizeof(int *) * height);
-
-        if (array == NULL)
-        return (NULL);
-
-        for (i = 0; i < height; i++)
-        {
-                array[i] = (int *)malloc(sizeof(int) * width);
-                if (array[i] == NULL)
-                {
+	for (i = 0; i < height; i++)
+	{
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
+		{
                         for (i--; i >= 0; i--)
 			free(array[i]);
 			free(array);
 			return (NULL);
 		}
 	}
-		for (i = 0; i < height; i++)
-		for (x = 0; x < width; x++)
+for (i = 0; i < height; i++)
+for (x = 0; x < width; x++)
 array[i][x] = 0;
 return (array);
 }
