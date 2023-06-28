926d0ed539e034c429a2a6174424d9553f3e82a7
TPoe25 <t_poe25@yahoo.com>
2023-06-27 17:13:44 -0700

    mul argc

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry
+ * @argc: arg count
+ * @argv: arg vector
+ * @a: 1st num
+ * @b: 2nd num
+ * @result: result
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a*b;
+
+	printf("%d\n", result);
+
+	return (0);
+}
