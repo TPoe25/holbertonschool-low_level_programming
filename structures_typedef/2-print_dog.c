27ed753fb25a1a41a472e55c0bf4a34e3519ece5
TPoe25 <t_poe25@yahoo.com>
2023-07-04 14:51:25 -0700

    2 fix

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -4,7 +4,6 @@
 
 /**
  * print_dog - entry
- * @dog: structure
  * @d: pointer
  */
 
@@ -15,11 +14,11 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
-			printf("Name: %s\n", d-> name);
-		printf("Age: %f\n", d->age);
+			printf("Name: %s\n", d -> name);
+		printf("Age: %f\n", d -> age);
 		if (d->owner == NULL)
 			printf("Owner (nil)\n");
 		else
-			printf("Owner: %s\n", d->owner);
+			printf("Owner: %s\n", d -> owner);
 	}
 }
