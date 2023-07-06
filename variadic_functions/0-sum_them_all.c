1d114a002fd6da5ee709c5f41f32e94388a0185c
TPoe25 <t_poe25@yahoo.com>
2023-07-05 17:19:47 -0700

    style fix 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,16 +11,16 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ap;
 	unsigned int i, sum;
 
-	va_start (ap, n);
+	va_start(ap, n);
 
 	sum = 0;
-	
+
 	if (n == 0)
 		return (0);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg (ap, const unsigned int);
+		sum += va_arg(ap, const unsigned int);
 
-	va_end (ap);
-	return sum;
+	va_end(ap);
+	return (sum);
 }
