4b3cbd81c6a677946f79cdf983410d40a2657d24
TPoe25 <t_poe25@yahoo.com>
2023-07-05 18:16:19 -0700

    1print

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: const char
+ * @n: int
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int at;
+	unsigned int x = 0;
+
+	va_start(ap, n);
+	for (at = 0; at < n; at++)
+	{
+		x = va_arg(ap,const unsigned int);
+			printf("%d", x);
+
+		if (x < (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+
+	va_end(ap);
+}
