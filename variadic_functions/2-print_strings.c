0f34b8d047a5212efcee13b3486253693aefd61f
TPoe25 <t_poe25@yahoo.com>
2023-07-05 21:43:02 -0700

    2 print

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: separator between the strings
+ * @n: # of args
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *string;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		string = va_arg(ap, char *);
+		if(!string)
+			string = "nil";
+		if (!separator)
+			printf("%s", string);
+		else if (separator && i == 0)
+			printf("%s", string);
+		else
+			printf("%s%s", separator, string);
+	}
+	printf("\n");
+	va_end(ap);
+}
