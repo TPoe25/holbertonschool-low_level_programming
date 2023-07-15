8758169e4c689a7f89a34c2e1bb64dd790e08465
TPoe25 <t_poe25@yahoo.com>
2023-07-15 14:08:07 -0700

    #2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: parameter
+ * @str: parameter
+ * Return: Result
+ */
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	
+	if (str == NULL)
+		return (NULL);
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
