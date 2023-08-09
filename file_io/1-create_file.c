b5d5ce5e3c50b02c0eb5e105c640d846ff700dc3
TPoe25 <t_poe25@yahoo.com>
2023-08-09 13:43:36 -0700

    last

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,6 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file_descriptor, bytes_written, index = 0;
+	
 	if (filename == NULL)
 		return (-1);
 	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
