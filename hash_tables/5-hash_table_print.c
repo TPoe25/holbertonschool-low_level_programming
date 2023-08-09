17e00124868d1bca5f05917b9d70a5e0036f3ded
TPoe25 <t_poe25@yahoo.com>
2023-08-09 13:05:15 -0700

    5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,27 +1,26 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_print - function that prints hash table
  * @ht: hashtable
  **/
 void hash_table_print(const hash_table_t *ht)
 {
-	int i;
-
+	int i, comma = 0;
+	
 	if (ht == NULL)
-
 		return;
-
-	printf("\nHash Table\n-------------------\n");
-
+	printf("{");
 	for (i = 0; i < (int)ht->size; i++)
 	{
 		hash_node_t *node = ht->array[i];
 		while (node)
 		{
-			printf("Index:%d, Key:%s, Value:%s\n", i, node->key, node->value);
+			if (comma)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			comma = 1;
 			node = node->next;
 		}
 	}
-	printf("-------------------\n\n");
+	printf("}\n");
 }
