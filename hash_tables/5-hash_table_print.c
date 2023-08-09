cad48c12ce8790e53aed455951738a531cbafc3c
TPoe25 <t_poe25@yahoo.com>
2023-08-09 12:44:26 -0700

    fixed errors

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -4,18 +4,19 @@
  * hash_table_print - function that prints hash table
  * @ht: hashtable
  **/
-
 void hash_table_print(const hash_table_t *ht)
 {
 	int i;
 
 	printf("\nHash Table\n-------------------\n");
 
-	for (i = 0; i < ht->size; i++)
+	for (i = 0; i < (int)ht->size; i++)
 	{
-		if (ht->items[i])
+		hash_node_t *node = ht->array[i];
+		while (node)
 		{
-			printf("Index:%d, Key:%s, Value:%s\n", i table->items[i] -> key, table->items[i]->value;
+			printf("Index:%d, Key:%s, Value:%s\n", i, node->key, node->value);
+			node = node->next;
 		}
 	}
 
