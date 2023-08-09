9e8e29f18ad6228fa56bf7db216fc17974cc89fc
TPoe25 <t_poe25@yahoo.com>
2023-08-09 13:41:47 -0700

    fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,28 +1,30 @@
 #include "main.h"
 
+/**
+ * create_file - creates a file with specified content
+ * @filename: name of the file to create
+ * @text_content: content to write to the file
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
-	int file, bytes, index;
-
+	int file_descriptor, bytes_written, index = 0;
 	if (filename == NULL)
 		return (-1);
-
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (file == 0)
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file_descriptor == -1)
 		return (-1);
-
 	if (text_content != NULL)
 	{
-		for (index = 0; text_content[index] != '\0'; index++);
-
-		bytes = write(file, text_content, index);
-		if (bytes == 0)
+		while (text_content[index])
+			index++;
+		bytes_written = write(file_descriptor, text_content, index);
+		if (bytes_written == -1)
 		{
-			close(file);
+			close(file_descriptor);
 			return (-1);
 		}
 	}
-	close (file);
-
+	close(file_descriptor);
 	return (1);
 }
