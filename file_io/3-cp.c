a88ed18db4f00be69902f083686443d14e86db1b
TPoe25 <t_poe25@yahoo.com>
2023-08-09 14:09:29 -0700

    3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,45 +7,37 @@
  */
 int main(int ac, char **av)
 {
-	int fd_from, fd_to, bytes_read, bytes_written;
-	char buffer[1024];
+	int res;
+	ssize_t n;
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
-		exit(97); }
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
+
+	/* Read the content of the source file */
+	n = read_textfile(av[1], 1024);
+	if (n == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98); }
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+		exit(98);
+	}
+
+	/* Create the destination file and write the content to it */
+	res = create_file(av[2], NULL);
+	if (res == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		close(fd_from);
-		exit(99); }
-	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
+		exit(99);
+	}
+
+	res = append_text_to_file(av[2], av[1]);
+	if (res == -1)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99); } }
-	if (bytes_read == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		close(fd_from);
-		close(fd_to);
-		exit(98); }
-	if (close(fd_from) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100); }
-	if (close(fd_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100); }
-	return (0); }
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
+	return (0);
+}
